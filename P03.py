63d115c17a132aaa345d81a074cfea690d03a8c6
Edcastle171 <ecastillo171@student.ccc.edu>
2025-01-29 19:42:32 -0600

    Add files via upload
    UNFINISHED!!!!!!!

---
+++ P03.py
@@ -0,0 +1,18 @@
+pounds = float(input('How many pounds of screws will you be purchasing?'))
+sale = pounds * .99
+if pounds > 10 or < 100
+   disc = 10
+else:
+   if pounds >= 100 or < 1000
+       disc = 20
+   else:
+       if pounds >= 1000 or 9999.99
+           disc = 30
+       else:
+           if pounds >= 10000
+               disc = 40
+finalprice = (.99 % pounds) * disc * 100
+print('Number of pounds:', pounds)
+print('Gross Sales:'sale'$')
+print('Discount:', disc'$')
+print('Final Amount:',finalprice)
