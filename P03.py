cf35540f43a41aff0c9d05d1b6412e0dcbb74d76
Edcastle171 <ecastillo171@student.ccc.edu>
2025-02-10 19:04:39 -0600

    Update P03.py

--- P03.py
+++ P03.py
@@ -1,12 +1,12 @@
-quantity = int(input("Pounds of screws: "))
+quantity = float(input("Pounds of screws: "))
 
 if quantity <=0:
     print("Error: Please provide a number of pounds greater than 0")
 else:
     price = .99
-if 0 <= quantity <= 9.99:
-    discount = 0.0
-if 10 <= quantity <= 99.99:
+if 0 < quantity <= 9.99:
+    discount = 0
+elif 10 <= quantity <= 99.99:
     discount = 0.10
 elif 100 <= quantity <= 999.99:
     discount = 0.20
@@ -21,5 +21,5 @@ final_price = total - discount_amount
 
 print("Number of pounds: ", quantity)
 print("Gross sales: $", round(total, 2))
-print("Discount: $", discount_amount)
+print("Discount: $", round(discount_amount, 2))
 print("Final Amount $", round(final_price, 2))
