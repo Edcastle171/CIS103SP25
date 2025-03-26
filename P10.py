7da00d8cdaf152ebe238f2f9cef04efddd690c3b
Edcastle171 <ecastillo171@student.ccc.edu>
2025-03-25 22:27:41 -0500

    Update P10.py

--- P10.py
+++ P10.py
@@ -1,11 +1,11 @@
 def main():
     thetext = '''
-       Python was conceived in the late 1980’s by Netherlands programmer
+       Python was conceived in the late 1980's by Netherlands programmer
 Guido Van Rossum and rolled out in 1991. Developing the language
 was a hobby project for Van Rossum to keep him occupied over
 Christmas, though he soon began implementing the language at
 his employer Centrum Wiskunde & Informatica (CWI). The name of
-the language was inspired by Monty Python’s Flying Circus, and
+the language was inspired by Monty Python's Flying Circus, and
 today users of this code often work in references to Monty Python.
 Python is one of the most popular programming languages in the
 world. As a scripting language that can automate a complex series
@@ -23,6 +23,7 @@ language to support both front-end and back-end functionality.
     the_count = thetext.lower().count(" the ")
     print("Count of 'the':", the_count)
     
+
     if "little" in thetext:
         print("The word 'little' was found.")
     else:
@@ -35,7 +36,7 @@ language to support both front-end and back-end functionality.
     
     position = thetext.find("appl")
     print("Position of 'appl':", position if position != -1 else "Not found")
-
+    
     thetext2 = thetext
     
     thetext2 = thetext2.replace("Python", "PYTHON")
